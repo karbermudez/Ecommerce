730e7b3f471aeaa8ae20f3777dd7dbd32b5f34a2
karen bermudez <karu.bermudez@hotmail.com.ar>
2021-07-18 22:14:37 -0300

    add itemlistcontainer & cart

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css';
+import './style/App.css';
 import Navbar from "./Components/NavBar/Navbar";
+import CartWidget from './Components/CartWidget/CartWidget';
+import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar/>
+    <div className="App" style={{width:"970px", marginLeft: "auto", marginRight:"auto"}}>
+      <div className="navigation-wrapper">
+        <Navbar/>
+      <CartWidget/>
+      </div>
+      <ItemListContainer saludo="Bienvenidos!!!!"/>
     </div>
   );
 }
---
+++ my-app/src/Components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const   ItemListContainer = (props) => {
+
+return (
+    <nav>
+        <h1>{props.saludo}</h1>
+    </nav>
+);
+};
+
+export default ItemListContainer;
